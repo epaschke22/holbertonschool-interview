b12c929d21f0f6098d6e8550a5758fb5cf675ab3
epaschke22 <ericksonpaschke@gmail.com>
2021-07-21 19:15:11 +0000

    submitting task 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,6 +2,23 @@
 """Lockboxes"""
 
 
+from os import truncate
+
+
 def canUnlockAll(boxes):
     """boxes is a list of lists"""
-    
+    if len(boxes) == 0:
+        return False
+    openboxes = [False] * len(boxes)
+    openboxes[0] = True
+    recursion(boxes[0], boxes, openboxes)
+    for box in openboxes:
+        if box is False:
+            return False
+    return True
+
+def recursion(boxindex, boxes, openboxes):
+    for key in boxindex:
+        if openboxes[key] == False and key < len(boxes):
+            openboxes[key] = True
+            recursion(boxes[key], boxes, openboxes)
