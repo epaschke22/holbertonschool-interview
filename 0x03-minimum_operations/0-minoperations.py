a2af085c6a30294964aec84ec9296232754c44f7
epaschke22 <ericksonpaschke@gmail.com>
2021-08-11 18:53:10 +0000

    updating task

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,7 +4,7 @@
 
 def minOperations(n):
     """return # of operations to copypaste 1 character to have n ammount"""
-    if n < 1:
+    if n < 2:
         return 0
     output = 0
     for i in range(2, n + 1):
