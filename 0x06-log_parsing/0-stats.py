629ed59092947aaf858f1e41ea607ff7ec03540a
epaschke22 <ericksonpaschke@gmail.com>
2021-09-02 02:34:00 +0000

    updating task

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,7 +3,7 @@ import sys
 
 
 def printvalues(stats, size):
-    statarr = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    statarr = [200, 301, 400, 401, 403, 404, 405, 500]
     print("File size: {}".format(size))
     for i in range(len(statarr)):
         if stats[statarr[i]] > 0:
@@ -11,13 +11,13 @@ def printvalues(stats, size):
 
 count = 0
 size = 0
-statdict = {"200": 0, "301": 0, "400": 0, "401": 0,
-            "403": 0, "404": 0, "405": 0, "500": 0}
+statdict = {200: 0, 301: 0, 400: 0, 401: 0,
+            403: 0, 404: 0, 405: 0, 500: 0}
 for line in sys.stdin:
     count += 1
     linesplit = line.split()
     size += int(linesplit[-1])
-    status = linesplit[-2]
+    status = int(linesplit[-2])
     statdict[status] += 1
     if count == 10:
         count = 0
