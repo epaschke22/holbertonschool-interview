d191aa2583683495a75a7e36420a3c6c17e1a7b9
epaschke22 <ericksonpaschke@gmail.com>
2021-09-22 18:02:38 +0000

    updating task

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,8 +3,33 @@
 
 
 def validUTF8(data):
-    """a character in UTF-8 can be 1 to 4 bytes long"""
-    for char in data:
-        if char > 255:
+    """UTF-8 hasa lot of rules"""
+    length = len(data)
+    if length == 1:
+        if data[0] < 128:
+            return True
+        else:
             return False
+    checkahead = 0
+    for char in data:
+        if checkahead > 0:
+            if char < 128 or char >= 192:
+                return False
+            checkahead -= 1
+        if char >= 192 and char < 224:
+            if checkahead > 0:
+                return False
+            checkahead = 1
+        if char >= 224 and char < 240:
+            if checkahead > 0:
+                return False
+            checkahead = 2
+        if char >= 240 and char < 248:
+            if checkahead > 0:
+                return False
+            checkahead = 3
+        if char >= 248:
+            return False
+    if checkahead > 0:
+        return False
     return True
