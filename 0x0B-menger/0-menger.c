5b27b3a03d0373a7a46648ba6825b9fd6d50d9a5
epaschke22 <ericksonpaschke@gmail.com>
2021-10-06 23:11:24 +0000

    updated betty fix

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -7,14 +7,19 @@
  */
 void menger(int level)
 {
-	int i, j, dim, d;
+	int i = 0, j, d, dim = 1;
 
 	if (level < 0)
 		return;
-	for (i = 0, dim = 1; i < level; i++, dim *= 3);
- 
-	for (i = 0; i < dim; i++) {
-		for (j = 0; j < dim; j++) {
+	while (i < level)
+	{
+		i++;
+		dim *= 3;
+	}
+	for (i = 0; i < dim; i++)
+	{
+		for (j = 0; j < dim; j++)
+		{
 			for (d = dim / 3; d; d /= 3)
 				if ((i % (d * 3)) / d == 1 && (j % (d * 3)) / d == 1)
 					break;
