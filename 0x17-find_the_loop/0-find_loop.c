11055e330189e397fe17287878223a930281c3a6
epaschke22 <ericksonpaschke@gmail.com>
2022-01-12 22:40:35 -0500

    submitting files for project

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds lopp in linked list
+ * @head: input list
+ * Return: node that starts loop, else NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *head1, *head2;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+	head1 = head;
+	head2 = head->next;
+	while (head2 != NULL)
+	{
+		if (head1 == head2)
+			return (head2);
+		printf("%d, %d\n", head1->n, head2->n);
+		head1 = head1->next;
+		head2 = head2->next;
+
+		if (head2 == NULL)
+			break;
+
+		if (head1 == head2)
+			return (head2);
+		printf("%d, %d\n", head1->n, head2->n);
+		head2 = head2->next;
+	}
+	return (NULL);
+}
