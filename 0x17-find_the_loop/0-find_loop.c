8bd1b6571f6ae92aee1bcb6d90f44575a5a0d9fb
epaschke22 <ericksonpaschke@gmail.com>
2022-01-13 00:58:39 -0500

    updating task

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -11,21 +11,22 @@ listint_t *find_listint_loop(listint_t *head)
 
 	if (head == NULL || head->next == NULL)
 		return (NULL);
-	head1 = head;
-	head2 = head->next;
-	while (head2 != NULL)
+	head1 = head->next;
+	head2 = head->next->next;
+	while (head2 && head2->next)
 	{
 		if (head1 == head2)
-			return (head2);
+			break;
+		head1 = head1->next;
+		head2 = head2->next->next;
+	}
+	if (head1 != head2)
+		return (NULL);
+	head1 = head;
+	while (head1 != head2)
+	{
 		head1 = head1->next;
 		head2 = head2->next;
-
-		if (head2 == NULL)
-			break;
-
-		if (head1 == head2)
-			return (head2);
-		head2 = head2->next;
 	}
-	return (NULL);
+	return (head1);
 }
