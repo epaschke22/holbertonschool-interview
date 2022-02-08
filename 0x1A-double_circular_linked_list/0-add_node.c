369cf4ec75bac486e7f5d76f3578ba41bf8d5873
epaschke22 <ericksonpaschke@gmail.com>
2022-02-08 15:35:06 -0500

    submitting files for task

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+/**
+ * add_node_end - Adds node to end of double circular linked list
+ * @list: A pointer to the head of the linkd list
+ * @str: string to copy into new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+
+}
+
+/**
+ * add_node_begin - Adds node to beginning of double circular linked list
+ * @list: A pointer to the head of the linkd list
+ * @str: string to copy into new node
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,2 @@
+# 0x1A. Double Circular Linked List
+https://intranet.hbtn.io/projects/478
