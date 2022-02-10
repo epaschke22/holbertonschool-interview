e51215e9de3c39b8f9f4f66936bb1fda01a7edf4
epaschke22 <ericksonpaschke@gmail.com>
2022-02-09 19:05:22 -0500

    trying strdup again

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -16,7 +16,7 @@ List *add_node_end(List **list, char *str)
 	new = malloc(sizeof(List));
 	if (new == NULL)
 		return (NULL);
-	new->str = str;
+	new->str = strdup(str);
 	new->next = new;
 	new->prev = new;
 
@@ -49,7 +49,7 @@ List *add_node_begin(List **list, char *str)
 	new = malloc(sizeof(List));
 	if (new == NULL)
 		return (NULL);
-	new->str = str;
+	new->str = strdup(str);
 	new->next = new;
 	new->prev = new;
 
