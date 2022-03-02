74c19ed7772a2ddc59a697dde6c5e8bd7ca7008b
epaschke22 <ericksonpaschke@gmail.com>
2022-03-02 18:43:39 -0500

    adding files for task

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * max - returns the higher of 2 values
+ * @a: first int
+ * @b: second int
+ * Return: which value is bigger
+ */
+int max(int a, int b)
+{
+	return ((a >= b) ? a : b);
+}
+
+/**
+ * height - returns the height of the tree
+ * @node: current node to check for
+ * Return: int amount of nodes returned
+ */
+int height(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	return (1 + max(height(node->left), height(node->right)));
+}
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is a vaild AVL tree
+ * @tree: a pointer to the root node of the tree
+ * Return: 1 if tree is valid, else 0
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int lh, rh;
+
+	if (tree == NULL)
+		return (0);
+
+	lh = height(tree->left);
+	rh = height(tree->right);
+
+	if (abs(lh - rh) <= 1 && binary_tree_is_avl(tree->left) &&
+							 binary_tree_is_avl(tree->right))
+		return (1);
+
+	return (0);
+}
---
+++ 0x1D-avl_trees/README.md
@@ -0,0 +1,2 @@
+# 0x1D. AVL Trees
+https://intranet.hbtn.io/projects/484
