e2229f0c0b121d6de575f4eb9b51843084f9b38b
epaschke22 <ericksonpaschke@gmail.com>
2022-04-07 15:24:43 -0400

    updated and restructured code to simply check for prime numbers

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -2,32 +2,13 @@
 """Prime Game"""
 
 
-def isprime(n):
-    """determines if a number is prime"""
-    if n <= 1:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
 def roundWinner(n):
     """determines the winner of the round"""
     turn = 0
-    set = []
     for i in range(1, n + 1):
-        set.append(i)
-    for i in range(1, len(set)):
-        if (isprime(i)):
-            for num in set:
-                if num % i == 0:
-                    set.remove(num)
+        if i in [1, 2, 3] or i % 6 == 1 or i % 6 == 5:
             turn += 1
-        i += 1
-    if turn % 2 == 0:
-        return 1
-    return -1
+    return 1 if turn % 2 == 1 else -1
 
 
 def isWinner(x, nums):
@@ -35,8 +16,4 @@ def isWinner(x, nums):
     winner = 0
     for i in range(x):
         winner += roundWinner(nums[i])
-    if winner == 0:
-        return None
-    if winner > 0:
-        return "Ben"
-    return "Maria"
+    return None if winner == 0 else "Ben" if winner > 0 else "Maria"
