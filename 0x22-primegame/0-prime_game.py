c5bc8ad519f53d9adf716eb78a2abb11691718b7
epaschke22 <ericksonpaschke@gmail.com>
2022-04-07 15:04:01 -0400

    fixed pep8 issue

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -6,7 +6,7 @@ def isprime(n):
     """determines if a number is prime"""
     if n <= 1:
         return False
-    for i in range(2,n):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
