ddb264e5839b4c8727a652cddc7c1f2b13717bdd
epaschke22 <ericksonpaschke@gmail.com>
2022-04-07 15:30:49 -0400

    restructured code again to be more compact

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -2,18 +2,13 @@
 """Prime Game"""
 
 
-def roundWinner(n):
-    """determines the winner of the round"""
-    turn = 0
-    for i in range(1, n + 1):
-        if i in [1, 2, 3] or i % 6 == 1 or i % 6 == 5:
-            turn += 1
-    return 1 if turn % 2 == 1 else -1
-
-
 def isWinner(x, nums):
     """Maria and Ben take turns choosing prime numbers"""
     winner = 0
-    for i in range(x):
-        winner += roundWinner(nums[i])
+    for round in range(x):
+        prime = 0
+        for n in range(1, nums[round] + 1):
+            if n in [1, 2, 3] or n % 6 == 1 or n % 6 == 5:
+                prime += 1
+        winner += 1 if prime % 2 == 1 else -1
     return None if winner == 0 else "Ben" if winner > 0 else "Maria"
