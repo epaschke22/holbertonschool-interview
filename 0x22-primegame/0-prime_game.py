a5715a305a8c6163e6567398a3c3f339d4889823
epaschke22 <ericksonpaschke@gmail.com>
2022-04-07 23:09:52 -0400

    added more robust way to check if numbers are prime

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
 """Prime Game"""
+from math import sqrt
+
+
+def isPrime(n):
+    """determines if a number is prime"""
+    prime_flag = 0
+    if(n > 1):
+        for i in range(2, int(sqrt(n)) + 1):
+            if (n % i == 0):
+                prime_flag = 1
+                break
+        return True if prime_flag == 0 else False
+    else:
+        return False
 
 
 def isWinner(x, nums):
     """Maria and Ben take turns choosing prime numbers"""
     winner = 0
     for round in range(x):
-        prime = 0
+        prime = 1
         for n in range(1, nums[round] + 1):
-            if n in [1, 2, 3] or n % 6 == 1 or n % 6 == 5:
+            if isPrime(n):
                 prime += 1
         winner += 1 if prime % 2 == 1 else -1
     return None if winner == 0 else "Ben" if winner > 0 else "Maria"
