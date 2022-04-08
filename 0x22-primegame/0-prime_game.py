8bdf56c01b19e1dbc16c46186bb6f9b934447748
epaschke22 <ericksonpaschke@gmail.com>
2022-04-07 23:16:50 -0400

    changed prime method to not require import

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """Prime Game"""
-from math import sqrt
 
 
-def isPrime(n):
+def isPrime(num):
     """determines if a number is prime"""
-    prime_flag = 0
-    if(n > 1):
-        for i in range(2, int(sqrt(n)) + 1):
-            if (n % i == 0):
-                prime_flag = 1
-                break
-        return True if prime_flag == 0 else False
+    if num > 1:
+        for i in range(2, int(num / 2) + 1):
+            if (num % i) == 0:
+                return False
+        else:
+            return True
     else:
         return False
 
